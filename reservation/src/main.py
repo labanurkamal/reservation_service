98ac3977e2a16917adee171788b718767c646ac8
Aibek <154282821+labanurkamal@users.noreply.github.com>
2025-04-15 22:35:11 +0500

    Add main file.

---
+++ reservation/src/main.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from api.v1 import api_v1_router
+from core.config import settings
+from dependencies.container import Container
+from exceptions.base import AppException
+from exceptions.handlers import app_exception_handler
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    container: Container = Container()
+    app.container = container
+
+    yield
+app = FastAPI(
+    lifespan=lifespan,
+    title=settings.project_name,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+    version=settings.version,
+)
+
+app.add_exception_handler(AppException, app_exception_handler)
+app.include_router(api_v1_router, prefix='/api/v1')
