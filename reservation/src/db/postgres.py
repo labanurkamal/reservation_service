303150e238ce4c60e37ff0b6012522bda9cb82ab
Aibek <154282821+labanurkamal@users.noreply.github.com>
2025-04-15 22:27:37 +0500

    Add database SQLALCHEMY.

---
+++ reservation/src/db/postgres.py
@@ -0,0 +1,19 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from core.config import db_settings
+
+class Database:
+    def __init__(self, db_url: str, echo: bool = False):
+        self.engine = create_async_engine(db_url, echo=echo)
+        self.session_factory = async_sessionmaker(bind=self.engine, class_=AsyncSession, expire_on_commit=False)
+
+    async def session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            try:
+                yield session
+            except Exception:
+                await session.rollback()
+                raise
+
+db = Database(db_url=db_settings.dsn, echo=db_settings.echo)
